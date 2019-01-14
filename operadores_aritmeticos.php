50e5be34c523ca767928e53f4e28ccbe7aa86594
Junior Veras <45885190+Juniorveras@users.noreply.github.com>
2019-01-14 12:44:17 -0200

    Add files via upload

---
+++ operadores_aritmeticos.php
@@ -0,0 +1,28 @@
+
+<?php 
+// + Adição
+// - Subtração
+// / Divisão
+// * Multiplicação
+// % Modulo OBS: Resto da DIVISÃO.
+
+// EX:
+
+$n1 = 7;
+$n2 = 3;
+
+echo $n1 + $n2;
+echo '<br>';
+echo $n1 - $n2;
+echo '<br>';
+echo $n1 / $n2;
+echo '<br>';
+echo $n1 * $n2;
+echo '<br>';
+echo $n1 % $n2;
+echo '<br>';
+
+//teste executado.
+
+?>
+
